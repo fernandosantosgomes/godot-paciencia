fdd363b53aef3e9655d4301924af441fdc34be36
Fernandosgomes <Fernandosg011@gmail.com>
2022-06-10 17:04:21 -0300

    Adicionada funlçao de carregamento de textura para cada carta

--- src/baralho.gd
+++ src/baralho.gd
@@ -6,9 +6,9 @@ var Carta = preload("res://src/carta.tscn")
 
 var deck
 
-enum naipe { COPAS,	PAUS, OURO,	ESPADAS}
+enum naipe { Hearts, Spades, Diamonds,	Clubs}
 
-enum valor {CURINGA, AS, DOIS, TREZ, QUATRO, CINCO, SEIS, SETE, OITO, NOVE, DEZ, VALETE, RAINHA, REI}
+enum valor {Joker, _A, _2, _3, _4, _5, _6, _7, _8, _9, _10, _J, _Q, _K}
 
 func _init():
 	preenche_deck()
@@ -17,10 +17,12 @@ func preenche_deck():
 	deck = []
 	var carta
 	for chave in valor:
-		if chave == 'CURINGA':
+		if chave == 'Joker':
 			continue
 		for np in naipe:
 			carta = Carta.instance()
-			carta.valor = chave
+			carta.valor = chave.substr(1)
 			carta.naipe = np
+			var textura =  load("res://assets/cartas/card" + carta.naipe + carta.valor +".png")
+			carta.textura_desvirada = textura
 			deck.append(carta)
--- src/carta.gd
+++ src/carta.gd
@@ -4,8 +4,8 @@ var valor
 var naipe
 var virada = false
 var cor_atraz
-var textura_desvirada =  load("res://assets/cartas/cardClubs3.png")
+var textura_desvirada
 var textura_virada
 
-func _ready():
+func _ready(): 
 	$Sprite.texture = textura_desvirada
