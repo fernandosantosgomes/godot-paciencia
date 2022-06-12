335cfcbf32d8e7c1d9074cdc94fe437bbcf2659f
Fernandosgomes <Fernandosg011@gmail.com>
2022-06-12 18:42:38 -0300

    Cartas prgavél com p mause

--- src/baralho/carta/carta.gd
+++ src/baralho/carta/carta.gd
@@ -6,6 +6,19 @@ var virada = false
 var cor_atraz
 var textura_desvirada
 var textura_virada
+var mouse_enter = false
 
 func _ready(): 
 	$Sprite.texture = textura_desvirada
+
+
+func _process(delta):
+	if Input.is_action_pressed("click"):
+		position = get_global_mouse_position()
+		
+func _on_Coliso_mouse_entered():
+	mouse_enter = true
+
+
+func _on_Coliso_mouse_exited():
+	mouse_enter = false
